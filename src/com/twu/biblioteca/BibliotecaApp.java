4384e617a617d635cf2b1cf05809371dde277c02
LoretoGutierrezH <loretocgh@gmail.com>
2021-02-12 14:06:04 -0300

    minor changes to swithc/case statements to allow different cases

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -9,35 +9,39 @@ public class BibliotecaApp {
         //Initializing application
         Scanner sc = new Scanner(System.in);
         System.out.println(Notification.welcome);
-        System.out.println("Menu options - books (show list of books), borrow (borrow a book), return (return a book)");
+        System.out.println("Menu options - 1- Books (show list of books), 2- Borrow (borrow a book), 3- Return (return a book)");
 
         //Waiting for user input and reacting accordingly
         while (true) {
             String userInput = sc.nextLine();
             switch(userInput) {
-                case "books": {
+                case "1":
+                case "Books":
+                case "books":
                     System.out.println(Library.getBooks());
                     break;
-                }
 
-                case "borrow": {
+                case "2":
+                case "Borrow":
+                case "borrow":
                     System.out.print(Notification.borrowOption);
                     userInput = sc.nextLine();
                     System.out.println(Library.checkoutBook(Integer.parseInt(userInput)));
                     break;
-                }
 
-                case "return": {
+                case "3":
+                case "Return":
+                case "return":
                     System.out.println(Notification.returnOption);
                     userInput = sc.nextLine();
                     System.out.println(Library.returnBook(Integer.parseInt(userInput)));
                     break;
-                }
 
-                case "quit": {
+                case "Quit":
+                case "quit":
                     System.out.println(Notification.onQuit);
                     break;
-                }
+
 
                 default: {
                     System.out.println(Notification.invalidOption);
@@ -45,7 +49,7 @@ public class BibliotecaApp {
                 }
             }
 
-           if (userInput.equals("quit")) {
+           if (userInput.equals("quit") || userInput.equals("Quit")) {
                 break;
            }
 
